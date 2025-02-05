2b0f0416142bc192af7327219e6c22c6ae6ad7a2
Test <ksyuzlo7073@gmail.com>
2025-02-05 12:34:29 +1000

    scndcmmt

--- dbhw1.sql
+++ dbhw1.sql
@@ -15,22 +15,22 @@ year smallint check (year > 0)
 )
 
 create table if not exists executorsgenres(
-executor_id int not null foreign key references executors(id),
-genre_id int not null foreign key references genres(id),
+executor_id int not null references executors(id),
+genre_id int not null references genres(id),
 constraint pk primary key (executor_id, genre_id)
 )
 
 create table if not exists executorsalbums(
-executor_id int not null foreign key references executors(id),
-album_id int not null foreign key references albums(id),
-constraint pk primary key (executor_id, album_id)
+executor_id int not null references executors(id),
+album_id int not null references albums(id),
+constraint ea_pk primary key (executor_id, album_id)
 )
 
 create table if not exists songs(
 id serial primary key,
 name varchar(50) not null,
 duration time not null,
-album_id int not null foreign key references album(id)
+album_id int not null references albums(id)
 )
 
 create table if not exists collections(
@@ -40,7 +40,7 @@ year smallint check (year > 0)
 )
 
 create table if not exists songscollections(
-song_id int not null foreign key references songs(id),
-collection_id int not null foreign key references collections(id),
-constraint pk primary key (song_id, collection_id)
+song_id int not null references songs(id),
+collection_id int not null references collections(id),
+constraint sc_pk primary key (song_id, collection_id)
 )
